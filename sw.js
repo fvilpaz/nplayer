64c458d15fab87ffeabd6d97f5c8a33233c38ea7
fvilpaz <fvilpaz@gmail.com>
2026-09-07 20:31:55 +0200

    deploy: 20260907203155

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nplayer-20260907203034';
+const CACHE = 'nplayer-20260907203155';
 const BASE = self.registration.scope;
 const ASSETS = ['', 'index.html', 'manifest.json', 'src/app.js', 'src/style.css', 'assets/icon.png', 'assets/nando.jpg'].map(f => BASE + f);
 
