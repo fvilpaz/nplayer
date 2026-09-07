fd155183122cd80bfbe30ce0d44ba1539b436c29
fvilpaz <fvilpaz@gmail.com>
2026-09-07 20:09:02 +0200

    Restructure: src/ for js/css, assets/ for images

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE = 'nplayer-v1';
 const BASE = self.registration.scope;
-const ASSETS = ['', 'index.html', 'style.css', 'app.js', 'manifest.json', 'icon.png', 'nando.jpg'].map(f => BASE + f);
+const ASSETS = ['', 'index.html', 'manifest.json', 'src/app.js', 'src/style.css', 'assets/icon.png', 'assets/nando.jpg'].map(f => BASE + f);
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
