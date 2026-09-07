64eeb28c1ceb6937b99e50bda3da20e1a4e40383
fvilpaz <fvilpaz@gmail.com>
2026-09-07 20:20:19 +0200

    SW: ensure skipWaiting after cache fill to auto-update

--- sw.js
+++ sw.js
@@ -3,8 +3,7 @@ const BASE = self.registration.scope;
 const ASSETS = ['', 'index.html', 'manifest.json', 'src/app.js', 'src/style.css', 'assets/icon.png', 'assets/nando.jpg'].map(f => BASE + f);
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
 
 self.addEventListener('activate', e => {
